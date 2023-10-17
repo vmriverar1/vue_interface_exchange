6209a0921049171ba997efb07d112753c538578d
vmriverar1 <vmriverar1@gmail.com>
2023-10-17 02:40:03 -0500

    aplicación terminada

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <px-header />
+    <px-header :links="links" />
     <router-view class="container px-5 sm:px20 py-20 flex justify-center" />
   </main>
 </template>
@@ -10,6 +10,24 @@ import PxHeader from "@/components/PxHeader";
 export default {
   name: "App",
   components: { PxHeader },
+  data() {
+    return {
+      links: [
+        {
+          title: "BTC",
+          to: { name: "coin-detail", params: { id: "bitcoin" } },
+        },
+        {
+          title: "ETH",
+          to: { name: "coin-detail", params: { id: "ethereum" } },
+        },
+        {
+          title: "XRP",
+          to: { name: "coin-detail", params: { id: "ripple" } },
+        },
+      ],
+    };
+  },
 };
 </script>
 
